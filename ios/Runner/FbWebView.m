b5d93ad656ca6bf190a6ef767fe8809bbdb088ff
Brett <brett@Bretts-Macintosh.local>
2018-08-23 14:00:16 -0500

    yay new macbook

--- ios/Runner/FbWebView.m
+++ ios/Runner/FbWebView.m
@@ -10,6 +10,8 @@
 #import <WebKit/WebKit.h>
 
 
+
+
 @interface FbWebView ()
 
 
@@ -20,6 +22,8 @@
 @property (strong, nonatomic)  UIButton *closeBtn;
 @property (strong, nonatomic)  UIProgressView *progressBar;
 @property (strong, nonatomic)  NSTimer *progressTimer;
+@property (strong, nonatomic)  UIView *loadingView;
+
 @property ( nonatomic)  bool finishedLoading;
 
 
@@ -48,16 +52,20 @@
 
 
 -(void)setupLoadingBar{
-    self.progressBar = [[UIProgressView alloc]initWithFrame:CGRectMake(0.0, 50.0, self.view.frame.size.width, 50.0)];
-    self.progressBar.backgroundColor = [UIColor blueColor];
-    [self.view addSubview:_progressBar];
+//    self.progressBar = [[UIProgressView alloc]initWithFrame:CGRectMake(0.0, 50.0, self.view.frame.size.width, 50.0)];
+//    self.progressBar.backgroundColor = [UIColor blueColor];
+//    [self.view addSubview:_progressBar];
+    
+    _loadingView = [[UIView alloc]initWithFrame:CGRectMake(0.0, 0.0, self.view.frame.size.width, 50.0)];
+    [self.view addSubview:_loadingView];
+    _loadingView.backgroundColor = [UIColor colorWithRed:0.23 green:0.35 blue:0.60 alpha:1.0];
     _progressTimer = [NSTimer scheduledTimerWithTimeInterval:0.001 target:self selector:@selector(timerCallback) userInfo:nil repeats:YES];
 
 }
 
 - (void)addWeb{
     WKWebViewConfiguration *theConfiguration = [[WKWebViewConfiguration alloc] init];
-    _webView = [[WKWebView alloc] initWithFrame:CGRectMake(0.0, 50.0, self.view.frame.size.width, (self.view.frame.size.height - 100.0)) configuration:theConfiguration];
+    _webView = [[WKWebView alloc] initWithFrame:CGRectMake(0.0, 50.0, self.view.frame.size.width, (self.view.frame.size.height - 110.0)) configuration:theConfiguration];
     NSURL *nsurl=[NSURL URLWithString:_url];
     NSURLRequest *nsrequest=[NSURLRequest requestWithURL:nsurl];
     [_webView loadRequest:nsrequest];
@@ -66,21 +74,21 @@
 }
 
 - (void) addNav{
-    _bottomNavBar = [[UIView alloc]initWithFrame:CGRectMake(0.0, (self.webView.frame.size.height - 70.0), self.webView.frame.size.width, 70.0)];
+    _bottomNavBar = [[UIView alloc]initWithFrame:CGRectMake(0.0, (self.view.frame.size.height - 60.0), self.webView.frame.size.width, 60.0)];
     [_bottomNavBar setUserInteractionEnabled:true];
-    _bottomNavBar.backgroundColor = [[UIColor yellowColor] colorWithAlphaComponent:0.5f];
-    [_webView addSubview:_bottomNavBar];
+    _bottomNavBar.backgroundColor = [UIColor colorWithRed:0.23 green:0.35 blue:0.60 alpha:1.0];
+    [self.view addSubview:_bottomNavBar];
     
-    _fwdBtn = [[UIButton alloc]initWithFrame:CGRectMake(70.0,5.0 , 50.0, 50.0)];
-    [_fwdBtn setImage:[UIImage imageNamed:@"forwardArrow"] forState:UIControlStateNormal];
+    _fwdBtn = [[UIButton alloc]initWithFrame:CGRectMake(70.0,5.0 , 40.0, 40.0)];
+    [_fwdBtn setImage:[UIImage imageNamed:@"whiteForward"] forState:UIControlStateNormal];
     [_fwdBtn addTarget:self action:@selector(goFwd) forControlEvents:UIControlEventTouchUpInside];
 
-    _backBtn = [[UIButton alloc]initWithFrame:CGRectMake(10,5.0, 50.0, 50.0)];
-    [_backBtn setImage:[UIImage imageNamed:@"backArrow"] forState:UIControlStateNormal];
+    _backBtn = [[UIButton alloc]initWithFrame:CGRectMake(10,5.0, 40.0, 40.0)];
+    [_backBtn setImage:[UIImage imageNamed:@"whiteBack"] forState:UIControlStateNormal];
     [_backBtn addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
     
-    _closeBtn = [[UIButton alloc]initWithFrame:CGRectMake((self.view.frame.size.width - 70.0),5.0 , 50.0, 50.0)];
-    [_closeBtn setImage:[UIImage imageNamed:@"closeBlack"] forState:UIControlStateNormal];
+    _closeBtn = [[UIButton alloc]initWithFrame:CGRectMake((self.view.frame.size.width - 70.0),5.0 , 40.0, 40.0)];
+    [_closeBtn setImage:[UIImage imageNamed:@"whiteMultiply"] forState:UIControlStateNormal];
     [_closeBtn addTarget:self action:@selector(close) forControlEvents:UIControlEventTouchUpInside];
 
 
@@ -104,12 +112,12 @@
     
     if(_progressBar.progress >= 1.0){
         [_progressTimer invalidate];
-        [_progressBar removeFromSuperview];
+        [_loadingView removeFromSuperview];
         return;
     }
+    [_loadingView setFrame:CGRectMake(0.0, 0.0, (self.view.frame.size.width * _webView.estimatedProgress), 50.0)];
 
 
-            _progressBar.progress = self.webView.estimatedProgress;
    
 
 }
